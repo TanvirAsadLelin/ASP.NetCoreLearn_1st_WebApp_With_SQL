dbb8ad91c527d3bda396e30f90a4b654c0fed29f
Tanvir Asad Lelin <md.tanvirasad.lelin@gmail.com>
2022-01-12 02:00:24 +0600

    Create database and add it to the project by using migrations and entityframework tools also

---
+++ ASP.NetCoreLearn-_WebApp_With_SQL/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ASP.NetCoreLearn__WebApp_With_SQL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NetCoreLearn__WebApp_With_SQL.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
--- ASP.NetCoreLearn-_WebApp_With_SQL/Program.cs
+++ ASP.NetCoreLearn-_WebApp_With_SQL/Program.cs
@@ -1,8 +1,13 @@
+using ASP.NetCoreLearn__WebApp_With_SQL.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options=> options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnnection")
+    ));
 
 var app = builder.Build();
 
