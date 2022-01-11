259f174c2c2aae1f09b756ec57527359ea7c9339
Tanvir Asad Lelin <md.tanvirasad.lelin@gmail.com>
2022-01-11 23:53:21 +0600

    System.ComponentModel.DataAnnotations; and add key attribute for primary key in class Category.cs

--- ASP.NetCoreLearn-_WebApp_With_SQL/Models/Category.cs
+++ ASP.NetCoreLearn-_WebApp_With_SQL/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace ASP.NetCoreLearn__WebApp_With_SQL.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NetCoreLearn__WebApp_With_SQL.Models
 {
     public class Category
-    {
+    {   
+        [Key]
         public int Id { get; set; }
+        [Required]  
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
 
