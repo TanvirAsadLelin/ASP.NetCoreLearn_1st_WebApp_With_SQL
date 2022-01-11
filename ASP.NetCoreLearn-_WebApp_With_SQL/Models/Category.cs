50034e96865a00611dd7e858ff07fca09e71010e
Tanvir Asad Lelin <md.tanvirasad.lelin@gmail.com>
2022-01-11 23:45:57 +0600

    Add a class in models for data name Category.cs

---
+++ ASP.NetCoreLearn-_WebApp_With_SQL/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NetCoreLearn__WebApp_With_SQL.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
