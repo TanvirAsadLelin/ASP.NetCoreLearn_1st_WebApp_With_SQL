5d0989b907614fc3a4ddf465aa3a6d96ecafc4b4
Tanvir Asad Lelin <md.tanvirasad.lelin@gmail.com>
2022-01-12 18:19:45 +0600

    Insert data in database and then show those data in Category table in web view

--- ASP.NetCoreLearn-_WebApp_With_SQL/Controllers/CategoryController.cs
+++ ASP.NetCoreLearn-_WebApp_With_SQL/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP.NetCoreLearn__WebApp_With_SQL.Data;
+using ASP.NetCoreLearn__WebApp_With_SQL.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NetCoreLearn__WebApp_With_SQL.Controllers
-{
+{      
+    
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
