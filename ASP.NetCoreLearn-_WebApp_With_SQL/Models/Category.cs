ddc1ecc612c4e09164930b71574707afc3d6d2bf
Tanvir Asad Lelin <md.tanvirasad.lelin@gmail.com>
2022-01-13 01:33:21 +0600

    Data edit is sucsessfuly completed and also add update button also

--- ASP.NetCoreLearn-_WebApp_With_SQL/Models/Category.cs
+++ ASP.NetCoreLearn-_WebApp_With_SQL/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASP.NetCoreLearn__WebApp_With_SQL.Models
 {
@@ -8,6 +9,8 @@ namespace ASP.NetCoreLearn__WebApp_With_SQL.Models
         public int Id { get; set; }
         [Required]  
         public string Name { get; set; }
+        [DisplayName ("Display Order")]
+        [Range(1,100, ErrorMessage ="Display order must be between 1 to 100 only")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
